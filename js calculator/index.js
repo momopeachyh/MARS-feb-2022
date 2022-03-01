aebe64008b34ff5e5eac32b5abd106ed6e7da0f0
Heidi <heidiw@marsreturnship.com>
2022-03-01 17:03:25 -0600

    Added subtraction function

--- js calculator/index.js
+++ js calculator/index.js
@@ -1,5 +1,4 @@
 /* Addition function */
-
 function calculateSum(event) {
   event.preventDefault();
   let addition1 = document.querySelector("#addition1");
@@ -11,3 +10,17 @@ function calculateSum(event) {
 
 let additionEquals = document.querySelector("#additionEquals");
 additionEquals.addEventListener("click", calculateSum);
+
+/*Subtraction function */
+function calculateDifference(event) {
+  event.preventDefault();
+  let subtraction1 = document.querySelector("#subtraction1");
+  let subtraction2 = document.querySelector("#subtraction2");
+  let subtractionAnswer =
+    Number(subtraction1.value) - Number(subtraction2.value);
+  let difference = document.querySelector("#difference");
+  difference.value = subtractionAnswer;
+}
+
+let subtractionEquals = document.querySelector("#subtractionEquals");
+subtractionEquals.addEventListener("click", calculateDifference);
