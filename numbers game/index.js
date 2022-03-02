0d943d80352e4134489dd4a40a0ec1cafe89b3fe
Heidi <heidiw@marsreturnship.com>
2022-03-02 16:54:13 -0600

    Fixes

--- numbers game/index.js
+++ numbers game/index.js
@@ -1,20 +1,35 @@
-let luckyNumber = 15;
-let guessInput = document.querySelector("#guessInput");
-let guessCount = 0;
+// let luckyNumber = 1;
+// let guessInput = document.querySelector("#guessInput");
+let guessCount = 12;
 
-function guessResponse(event) {
-  event.preventDefault();
+// function guessResponse(event) {
+//   event.preventDefault();
+//   guessCount++;
+//   if (guessInput.value == luckyNumber) {
+//     alert(`Correct! You guessed the lucky number in ${guessCount} tries.`);
+//     guessCount = 0;
+//   } else if (guessInput.value > 10) {
+//     alert("Please guess between 1 and 10.");
+//   } else {
+//     alert("Incorrect. Please guess again.");
+//   }
+//   guessInput.value = "";
+// }
+
+// let checkButton = document.querySelector("#checkButton");
+// checkButton.addEventListener("click", guessResponse);
+
+for (let i = 0; i < 10; i++) {
   guessCount++;
-  if (guessInput.value == luckyNumber) {
-    alert(`Correct! You guessed the lucky number in ${guessCount} tries!`);
-    guessCount = 0;
-  } else if (guessInput.value > 20) {
-    alert("Please guess between 0 and 20.");
-  } else {
-    alert("Incorrect. Please guess again.");
-  }
-  guessInput.value = "";
+  console.log(guessCount);
 }
 
-let checkButton = document.querySelector("#checkButton");
-checkButton.addEventListener("click", guessResponse);
+// function howMany(selectObject) {
+//   let numberSelected = 0;
+//   for (let i = 0; i < selectObject.options.length; i++) {
+//     if (selectObject.options[i].selected) {
+//       numberSelected++;
+//     }
+//   }
+//   return numberSelected;
+// }
