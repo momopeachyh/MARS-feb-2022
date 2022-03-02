6df0573518985ea103ab92bf06bc68d34e676d7b
Heidi <heidiw@marsreturnship.com>
2022-03-02 13:49:39 -0600

    Added guess counter

--- numbers game/index.js
+++ numbers game/index.js
@@ -1,9 +1,12 @@
 let luckyNumber = 15;
 let guessInput = document.querySelector("#guessInput");
+let guessCount = 0;
 
 function guessResponse(event) {
+  event.preventDefault();
+  guessCount++;
   if (guessInput.value == luckyNumber) {
-    alert("Correct! You guessed the lucky number!");
+    alert(`Correct! You guessed the lucky number in ${guessCount} tries!`);
   } else {
     alert("Incorrect. Please guess again.");
   }
