d670083612a5b451440f258c727ad6fcdbabe2f8
Heidi <heidiw@marsreturnship.com>
2022-03-02 18:38:45 -0600

    Made separate alert for correct first guesses

--- numbers game/index.js
+++ numbers game/index.js
@@ -1,28 +1,31 @@
-// let luckyNumber = 1;
-// let guessInput = document.querySelector("#guessInput");
-let guessCount = 12;
+let luckyNumber = 1;
+let guessInput = document.querySelector("#guessInput");
+let guessCount = 0;
 
-// function guessResponse(event) {
-//   event.preventDefault();
-//   guessCount++;
-//   if (guessInput.value == luckyNumber) {
-//     alert(`Correct! You guessed the lucky number in ${guessCount} tries.`);
-//     guessCount = 0;
-//   } else if (guessInput.value > 10) {
-//     alert("Please guess between 1 and 10.");
-//   } else {
-//     alert("Incorrect. Please guess again.");
-//   }
-//   guessInput.value = "";
-// }
-
-// let checkButton = document.querySelector("#checkButton");
-// checkButton.addEventListener("click", guessResponse);
-
-for (let i = 0; i < 10; i++) {
+function guessResponse(event) {
+  event.preventDefault();
   guessCount++;
-  console.log(guessCount);
+  if (guessInput.value == luckyNumber && guessCount == 1) {
+    alert(`Correct! You guessed the lucky number on your first try.`);
+    guessCount = 0;
+  } else if (guessInput.value == luckyNumber && guessCount !== 1) {
+    alert(`Correct! You guessed the lucky number in ${guessCount} tries.`);
+    guessCount = 0;
+  } else if (guessInput.value > 10) {
+    alert("Please guess between 1 and 10.");
+  } else {
+    alert("Incorrect. Please guess again.");
+  }
+  guessInput.value = "";
 }
+
+let checkButton = document.querySelector("#checkButton");
+checkButton.addEventListener("click", guessResponse);
+
+// for (let i = 0; i < 10; i++) {
+//   guessCount++;
+//   console.log(guessCount);
+// }
 
 // function howMany(selectObject) {
 //   let numberSelected = 0;
