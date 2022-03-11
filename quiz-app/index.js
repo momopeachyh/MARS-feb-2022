9c3899fc816c70c8d3734b568c7d55a28129d4ae
Heidi <heidiw@marsreturnship.com>
2022-03-11 09:26:48 -0600

    JS for feedback (question 2)

--- quiz-app/index.js
+++ quiz-app/index.js
@@ -1,28 +1,56 @@
-let button = document.querySelector("#button");
-// button.addEventListener("click", handleClick);
+// Question 1
+const card1 = document.querySelector("#card1");
 
-let question = document.querySelector("#questionCard");
-const card = document.querySelector("#card");
+function showFeedback1() {
+  const questionGuess = document.querySelector("#formSelect1").value;
+  const correctAlert = document.querySelector("#correctFeedback1");
+  const incorrectAlert = document.querySelector("#incorrectFeedback1");
 
-function showFeedback() {
-  const questionGuess = document.querySelector("#formSelect").value;
-  const correctAlert = document.querySelector("#correctFeedback");
-  const incorrectAlert = document.querySelector("#incorrectFeedback");
+  // Display "correct" feedback and change border color to green
   if (questionGuess === "correct") {
-    card.classList.remove("incorrect");
-    card.classList.add("correct");
+    card1.classList.remove("incorrect");
+    card1.classList.add("correct");
     correctAlert.classList.remove("hidden");
     incorrectAlert.classList.add("hidden");
+
+    // Display "incorrect" feedback and change border color to red
   } else if (questionGuess === "incorrect") {
-    card.classList.remove("correct");
-    card.classList.add("incorrect");
+    card1.classList.remove("correct");
+    card1.classList.add("incorrect");
     incorrectAlert.classList.remove("hidden");
     correctAlert.classList.add("hidden");
   }
 }
 
-function handleClick(event) {
-  // event.preventDefault();
+function handleClick1() {
   // questionCard.classList.add("hidden");
-  showFeedback();
+  showFeedback1();
+}
+
+// Question 2
+const card2 = document.querySelector("#card2");
+
+function showFeedback2() {
+  const questionGuess = document.querySelector("#formSelect2").value;
+  const correctAlert = document.querySelector("#correctFeedback2");
+  const incorrectAlert = document.querySelector("#incorrectFeedback2");
+
+  // Display "correct" feedback and change border color to green
+  if (questionGuess === "correct") {
+    card2.classList.remove("incorrect");
+    card2.classList.add("correct");
+    correctAlert.classList.remove("hidden");
+    incorrectAlert.classList.add("hidden");
+
+    // Display "incorrect" feedback and change border color to red
+  } else if (questionGuess === "incorrect") {
+    card2.classList.remove("correct");
+    card2.classList.add("incorrect");
+    incorrectAlert.classList.remove("hidden");
+    correctAlert.classList.add("hidden");
+  }
+}
+
+function handleClick2() {
+  showFeedback2();
 }
