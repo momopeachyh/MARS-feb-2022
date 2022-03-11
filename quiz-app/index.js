68abc9d4bc4e0ca5a811f296a04911afb2ea4f70
Heidi <heidiw@marsreturnship.com>
2022-03-11 09:15:59 -0600

    Change question layout and display answer feedback

--- quiz-app/index.js
+++ quiz-app/index.js
@@ -1,9 +1,28 @@
 let button = document.querySelector("#button");
-button.addEventListener("click", handleClick);
+// button.addEventListener("click", handleClick);
 
 let question = document.querySelector("#questionCard");
+const card = document.querySelector("#card");
+
+function showFeedback() {
+  const questionGuess = document.querySelector("#formSelect").value;
+  const correctAlert = document.querySelector("#correctFeedback");
+  const incorrectAlert = document.querySelector("#incorrectFeedback");
+  if (questionGuess === "correct") {
+    card.classList.remove("incorrect");
+    card.classList.add("correct");
+    correctAlert.classList.remove("hidden");
+    incorrectAlert.classList.add("hidden");
+  } else if (questionGuess === "incorrect") {
+    card.classList.remove("correct");
+    card.classList.add("incorrect");
+    incorrectAlert.classList.remove("hidden");
+    correctAlert.classList.add("hidden");
+  }
+}
 
 function handleClick(event) {
-  event.preventDefault();
-  questionCard.classList.add("hidden");
+  // event.preventDefault();
+  // questionCard.classList.add("hidden");
+  showFeedback();
 }
