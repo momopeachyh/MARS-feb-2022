3fd8da02ab29d83cecaaa65fddccd9cad44c52aa
Heidi <heidiw@marsreturnship.com>
2022-03-14 17:39:01 -0500

    Refactor questions into object and loop them through function

--- quiz-app/index.js
+++ quiz-app/index.js
@@ -1,84 +1,116 @@
-// Question 1
-const card1 = document.querySelector("#card1");
+const questionList = [
+  {
+    questNo: 1,
+    questText: "How do you say 'the horse' in German?",
+    a: "die Katze",
+    b: "der Hirsch",
+    c: "das Pferd",
+    d: "die Spinne",
+    correct: "das Pferd",
+  },
+  {
+    questNo: 2,
+    questText: "How do you say 'the cat' in German?",
+    a: "die Fledermaus",
+    b: "die Katze",
+    c: "die Spinne",
+    d: "der Hirsch",
+    correct: "die Katze",
+  },
+  {
+    questNo: 3,
+    questText: "How do you say 'the squirrel' in German?",
+    a: "das Eichhörnchen",
+    b: "der Hund",
+    c: "der Vogel",
+    d: "die Fledermaus",
+    correct: "das Eichhörnchen",
+  },
+  {
+    questNo: 4,
+    questText: "How do you say 'the spider' in German?",
+    a: "die spinne",
+    b: "der Schlange",
+    c: "die Fledermaus",
+    d: "der Vogel",
+    correct: "die Spinne",
+  },
+  {
+    questNo: 5,
+    questText: "How do you say 'the bat' in German?",
+    a: "der Schlange",
+    b: "der Hund",
+    c: "der Vogel",
+    d: "die Fledermaus",
+    correct: "die Fledermaus",
+  },
+];
 
-function showFeedback1() {
-  const questionGuess = document.querySelector("#formSelect1").value;
-  const correctAlert = document.querySelector("#correctFeedback1");
-  const incorrectAlert = document.querySelector("#incorrectFeedback1");
+const [quest1, quest2, quest3, quest4, quest5] = questionList;
 
-  // Display "correct" feedback and change border color to green
-  if (questionGuess === "correct") {
-    card1.classList.remove("incorrect");
-    card1.classList.add("correct");
-    correctAlert.classList.remove("hidden");
-    incorrectAlert.classList.add("hidden");
+const placeholderQuest = document.querySelector("#questText");
+const ansA = document.querySelector("#a");
+const ansB = document.querySelector("#b");
+const ansC = document.querySelector("#c");
+const ansD = document.querySelector("#d");
+const questContainer = document.querySelector("#questContainer");
+const correctAlert = document.querySelector("#correctFeedback");
+const incorrectAlert = document.querySelector("#incorrectFeedback");
 
-    // Display "incorrect" feedback and change border color to red
-  } else if (questionGuess === "incorrect") {
-    card1.classList.remove("correct");
-    card1.classList.add("incorrect");
-    incorrectAlert.classList.remove("hidden");
-    correctAlert.classList.add("hidden");
+let counter = 0;
+
+function showQuestion(question) {
+  placeholderQuest.innerHTML = `${question.questNo}. ${question.questText}`;
+  ansA.innerHTML = question.a;
+  ansB.innerHTML = question.b;
+  ansC.innerHTML = question.c;
+  ansD.innerHTML = question.d;
+
+  // Add value property to answer buttons
+  ansA.setAttribute("value", question.a);
+  ansB.setAttribute("value", question.b);
+  ansC.setAttribute("value", question.c);
+  ansD.setAttribute("value", question.d);
+
+  // Increment counter
+  counter++;
+
+  function showFeedback() {
+    const ansCorrect = question.correct;
+    const ansGuessed = this.value;
+
+    // Show feedback and change styling
+    if (ansGuessed == ansCorrect) {
+      correctAlert.classList.remove("hidden");
+      incorrectAlert.classList.add("hidden");
+      questContainer.classList.remove("incorrect");
+      questContainer.classList.add("correct");
+    } else {
+      incorrectAlert.classList.remove("hidden");
+      correctAlert.classList.add("hidden");
+      questContainer.classList.remove("correct");
+      questContainer.classList.add("incorrect");
+    }
   }
-}
 
-function handleClick1() {
-  // questionCard.classList.add("hidden");
-  showFeedback1();
-}
-
-// Question 2
-const card2 = document.querySelector("#card2");
-
-function showFeedback2() {
-  const questionGuess = document.querySelector("#formSelect2").value;
-  const correctAlert = document.querySelector("#correctFeedback2");
-  const incorrectAlert = document.querySelector("#incorrectFeedback2");
-
-  // Display "correct" feedback and change border color to green
-  if (questionGuess === "correct") {
-    card2.classList.remove("incorrect");
-    card2.classList.add("correct");
-    correctAlert.classList.remove("hidden");
-    incorrectAlert.classList.add("hidden");
-
-    // Display "incorrect" feedback and change border color to red
-  } else if (questionGuess === "incorrect") {
-    card2.classList.remove("correct");
-    card2.classList.add("incorrect");
-    incorrectAlert.classList.remove("hidden");
+  // Hide feedback and reset styling for next question
+  function resetStyling() {
     correctAlert.classList.add("hidden");
-  }
-}
-
-function handleClick2() {
-  showFeedback2();
-}
-
-// Question 3
-const card3 = document.querySelector("#card3");
-
-function showFeedback3() {
-  const questionGuess = document.querySelector("#formSelect3").value;
-  const correctAlert = document.querySelector("#correctFeedback3");
-  const incorrectAlert = document.querySelector("#incorrectFeedback3");
-
-  // Display "correct" feedback and change border color to green
-  if (questionGuess === "correct") {
-    card3.classList.remove("incorrect");
-    card3.classList.add("correct");
-    correctAlert.classList.remove("hidden");
     incorrectAlert.classList.add("hidden");
-
-    // Display "incorrect" feedback and change border color to red
-  } else if (questionGuess === "incorrect") {
-    card3.classList.remove("correct");
-    card3.classList.add("incorrect");
-    incorrectAlert.classList.remove("hidden");
-    correctAlert.classList.add("hidden");
+    questContainer.classList.remove("incorrect");
+    questContainer.classList.remove("correct");
   }
+
+  resetStyling();
+
+  ansA.addEventListener("click", showFeedback);
+  ansB.addEventListener("click", showFeedback);
+  ansC.addEventListener("click", showFeedback);
+  ansD.addEventListener("click", showFeedback);
 }
 
-function handleClick3() {
-  showFeedback3();
+function startQuiz() {
+  showQuestion(questionList[counter]);
 }
+
+startQuiz();
