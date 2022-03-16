0ec5114058e89676b91acb93413b4d7c2070af84
Heidi <heidi.willden@me.com>
2022-03-15 19:09:10 -0500

    Added addition functionality

--- react-calculator/src/Addition.js
+++ react-calculator/src/Addition.js
@@ -4,68 +4,31 @@ class Addition extends React.Component {
   constructor() {
     super();
     this.state = {
-      num1: "",
-      num2: "",
-      answer: "",
+      // num1: "",
+      // num2: "",
+      sum: "",
     };
   }
   render() {
     return (
       <div className="Addition">
         <form>
-          <input
-            type="number"
-            onChange={(event) => this.updateInputValue1(event)}
-          ></input>
+          <input type="number" id="add1" onChange={this.getNumbers}></input>
           <button>+</button>
-          <input
-            type="number"
-            onChange={(event) => this.updateInputValue2(event)}
-          ></input>
+          <input type="number" id="add2" onChange={this.getNumbers}></input>
           <input type="submit" value="=" onClick={this.sayHi}></input>
-          <input type="text" value={this.state.answer}></input>
+          <input type="text" value={this.state.sum}></input>
         </form>
       </div>
     );
   }
 
-  // getNumber = (event) => {
-  //   // event.preventDefault();
-  //   this.setState({ num1: event.target.value });
-  // };
-
-  updateInputValue1(event) {
-    const updatedNum1 = event.target.value;
-    this.setState({ num1: updatedNum1 });
-  }
-
-  updateInputValue2(event) {
-    const updatedNum2 = event.target.value;
-    this.setState({ num2: updatedNum2 });
-  }
-
-  sayHi = (event) => {
-    event.preventDefault();
-    console.log(this.num1, this.num2);
-    this.setState({ answer: "hi" });
+  getNumbers = () => {
+    let add1 = document.getElementById("add1").value;
+    let add2 = document.getElementById("add2").value;
+    let sum = Number(add1) + Number(add2);
+    this.setState({ sum: sum });
   };
 }
 
 export default Addition;
-
-//   render() {
-//     return (
-//       // ...
-//       <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
-//       // ...
-//     );
-//   },
-
-//   updateInputValue(evt) {
-//     const val = evt.target.value;
-//     // ...
-//     this.setState({
-//       inputValue: updatedInputValue
-//     });
-//   }
-// });
